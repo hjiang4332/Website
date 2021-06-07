f08439768903fc6f01bc02bc6221282f4d1caadd
Howard Jiang <hjiang4332@gmail.com>
2021-06-07 09:57:36 -0400

    update push

---
+++ experimentation/src/ClassIntro.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ClassIntro extends React.Component{
+    constructor(){
+        super()
+        this.state={
+            name: "hello",
+            age: "1337"
+        }
+    }
+}
+
+export default ClassIntro;
