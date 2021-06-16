e1d054bc510f874f864f12b8e35ed5b7bf6199c3
Howard Jiang <hjiang4332@gmail.com>
2021-06-16 15:23:00 -0400

    Finished Register Screen

--- Website/frontend/src/store.js
+++ Website/frontend/src/store.js
@@ -1,11 +1,13 @@
 import { compose, applyMiddleware, createStore, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
+
+//reducers
 import {
 	productDetailsReducer,
 	productListReducer,
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
-import { userSigninReducer } from './reducers/userReducers'
+import { userRegisterReducer, userSigninReducer } from './reducers/userReducers'
 
 //load previous items from cache / save stuff into local storage
 const initialState = {
@@ -21,11 +23,13 @@ const initialState = {
 	},
 }
 
+//reducers
 const reducer = combineReducers({
 	productList: productListReducer,
 	productDetails: productDetailsReducer,
 	cart: cartReducer,
 	userSignin: userSigninReducer,
+	userRegister: userRegisterReducer,
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
