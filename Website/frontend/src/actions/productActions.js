f5caec9619296b6db0e787ec16a062168358bf11
PTXD <hjiang4332@gmail.com>
2021-07-05 21:19:03 -0400

    installed multer for image upload and completed delete product

--- Website/frontend/src/actions/productActions.js
+++ Website/frontend/src/actions/productActions.js
@@ -3,6 +3,9 @@ import {
 	PRODUCT_CREATE_FAIL,
 	PRODUCT_CREATE_REQUEST,
 	PRODUCT_CREATE_SUCCESS,
+	PRODUCT_DELETE_FAIL,
+	PRODUCT_DELETE_REQUEST,
+	PRODUCT_DELETE_SUCCESS,
 	PRODUCT_DETAILS_FAIL,
 	PRODUCT_DETAILS_REQUEST,
 	PRODUCT_DETAILS_SUCCESS,
@@ -75,13 +78,13 @@ export const createProduct = () => async (dispatch, getState) => {
 //Product Edit Screen: edit products
 export const updateProduct = (product) => async (dispatch, getState) => {
 	dispatch({ type: PRODUCT_UPDATE_REQUEST, payload: product })
-    //get token
+	//get token
 	const {
 		userSignin: { userInfo },
 	} = getState()
 
 	try {
-        //destination, payload, options
+		//destination, payload, options
 		const { data } = await Axios.put(
 			`/api/products/${product._id}`,
 			product,
@@ -96,5 +99,26 @@ export const updateProduct = (product) => async (dispatch, getState) => {
 				? error.response.data.message
 				: error.message
 		dispatch({ type: PRODUCT_UPDATE_FAIL, error: message })
+	}
+}
+
+//ProductListScreen: delete product
+export const deleteProduct = (productId) => async (dispatch, getState) => {
+	dispatch({ type: PRODUCT_DELETE_REQUEST, payload: productId })
+	const {
+		userSignin: { userInfo },
+	} = getState()
+    
+	try {
+		const { data } = Axios.delete(`/api/products/${productId}`, {
+			headers: { Authorization: `Bearer ${userInfo.token}` },
+		})
+		dispatch({ type: PRODUCT_DELETE_SUCCESS })
+	} catch (error) {
+		const message =
+			error.response && error.response.data.message
+				? error.response.data.message
+				: error.message
+		dispatch({ type: PRODUCT_DELETE_FAIL, payload: message })
 	}
 }
--- Website/frontend/src/screens/ProductEditScreen.js
+++ Website/frontend/src/screens/ProductEditScreen.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { detailsProduct, updateProduct } from '../actions/productActions'
@@ -70,6 +71,33 @@ export default function ProductEditScreen(props) {
 			})
 		)
 	}
+
+	const [loadingUpload, setLoadingUpload] = useState(false)
+	const [errorUpload, setErrorUpload] = useState('')
+
+	const userSignin = useSelector((state) => state.userSignin)
+	const { userInfo } = userSignin
+
+	const uploadFileHandler = async (e) => {
+		const file = e.target.files[0]
+		const bodyFormData = new FormData()
+		bodyFormData.append('image', file)
+		setLoadingUpload(true)
+		try {
+			const { data } = await axios.post('/api/uploads', bodyFormData, {
+				headers: {
+					'Content-Type': 'multipart/form-data',
+					Authorization: `Bearer ${userInfo.token}`,
+				},
+			})
+			setImage(data)
+			setLoadingUpload(false)
+		} catch (error) {
+			setErrorUpload(error.message)
+			setLoadingUpload(false)
+		}
+	}
+
 	return (
 		<div>
 			<form className='form' onSubmit={submitHandler}>
@@ -119,6 +147,22 @@ export default function ProductEditScreen(props) {
 								value={image}
 								onChange={(e) => setImage(e.target.value)}
 							/>
+						</div>
+
+						<div>
+							<label htmlFor='imageFile'>Image File</label>
+							<input
+								type='file'
+								id='imageFile'
+								label='Choose Image'
+								onChange={uploadFileHandler}
+							></input>
+							{loadingUpload && <LoadingBox></LoadingBox>}
+							{errorUpload && (
+								<MessageBox variant='danger'>
+									{errorUpload}
+								</MessageBox>
+							)}
 						</div>
 
 						<div>
