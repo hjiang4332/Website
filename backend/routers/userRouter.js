a05bff3e86b019b127cc185feba224b95030ce80
Howard Jiang <hjiang4332@gmail.com>
2021-07-07 14:18:58 -0400

    Finished user list and edit

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -3,7 +3,7 @@ import expressAsyncHandler from 'express-async-handler'
 import bcrypt from 'bcryptjs'
 import data from '../data.js'
 import User from '../models/userModel.js'
-import { generateToken, isAuth } from '../utils.js'
+import { generateToken, isAdmin, isAuth } from '../utils.js'
 
 const userRouter = express.Router()
 
@@ -91,6 +91,57 @@ userRouter.put(
 				isAdmin: updatedUser.isAdmin,
 				token: generateToken(updatedUser),
 			})
+		}
+	})
+)
+
+//UserListScreen - get users
+userRouter.get(
+	'/',
+	isAuth,
+	isAdmin,
+	expressAsyncHandler(async (req, res) => {
+		const users = await User.find({})
+		res.send(users)
+	})
+)
+
+//UserListScreen - delete users
+userRouter.delete(
+	'/:id',
+	isAuth,
+	isAdmin,
+	expressAsyncHandler(async (req, res) => {
+		const user = await User.findById(req.params.id)
+		if (user) {
+			if (user.email === 'admin@example.com') {
+				res.status(400).send({ message: 'Can Not Delete Admin User' })
+				return
+			}
+			const deleteUser = await user.remove()
+			res.send({ message: 'User Deleted', user: deleteUser })
+		} else {
+			res.status(404).send({ message: 'User Not Found' })
+		}
+	})
+)
+
+//UserEditScreen - edit user information
+userRouter.put(
+	'/:id',
+	isAuth,
+	isAdmin,
+	expressAsyncHandler(async (req, res) => {
+		const user = await User.findById(req.params.id)
+		if (user) {
+			user.name = req.body.name || user.name
+			user.email = req.body.email || user.email
+			user.type = req.body.type || user.type
+			user.isAdmin = Boolean(req.body.isAdmin)
+			const updatedUser = await user.save()
+			res.send({ message: 'User Updated', user: updatedUser })
+		} else {
+			res.status(404).send({ message: 'User Not Found' })
 		}
 	})
 )
